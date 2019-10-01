acaee7cec2808f846d05a3500b3934b5c6f1155c
Eric W. Wallace <e@ewall.org>
2019-09-30 22:27:59 -0400

    four_peaks: adding timers and began tweaking

--- four_peaks.py
+++ four_peaks.py
@@ -2,62 +2,75 @@
 
 import mlrose
 import numpy as np
-import timeit
+import time
 
 fitness = mlrose.FourPeaks(t_pct=0.1)
-problem = mlrose.DiscreteOpt(length=100, fitness_fn=fitness, maximize=True, max_val=2)
 
-init_state = np.array(([0, 1] * 50))
-max_attempts = 10
-max_iters = 1000
+# for 4 Peaks of length 50, there are two local maxima of 50, and two optima of 94
+problem = mlrose.DiscreteOpt(length=50, fitness_fn=fitness, maximize=True, max_val=2)
+init_state = np.array(([0, 1] * 25))
+
+max_iters = 10000
 seed = 1
 
 # RHC
-rhc_restarts = 100
+rhc_restarts = 1000
+rhc_max_attempts = 30
+start_time = time.perf_counter()
 rhc_best_state, rhc_best_fitness, rhc_curve = mlrose.random_hill_climb(problem,
-                                                                       max_attempts=max_attempts,
+                                                                       max_attempts=rhc_max_attempts,
                                                                        max_iters=max_iters,
                                                                        restarts=rhc_restarts,
                                                                        init_state=init_state,
                                                                        curve=True,
                                                                        random_state=seed)
-#print('# RHC best state:\n', rhc_best_state)
-print('# RHC best fitness: ', rhc_best_fitness)
+rhc_time = time.perf_counter() - start_time
+print("RHC best fitness: {0:.0f} in {1:.4f} seconds and {2} iterations".format(rhc_best_fitness, rhc_time, len(rhc_curve)))
+# print('RHC best state:\n', rhc_best_state)
 
 # SA
 sa_schedule = mlrose.ArithDecay()
+sa_max_attempts = 30
+start_time = time.perf_counter()
 sa_best_state, sa_best_fitness, sa_curve = mlrose.simulated_annealing(problem,
                                                                       schedule=sa_schedule,
-                                                                      max_attempts=max_attempts,
+                                                                      max_attempts=sa_max_attempts,
                                                                       max_iters=max_iters,
                                                                       init_state=init_state,
                                                                       curve=True,
                                                                       random_state=seed)
-#print('# SA best state:\n', sa_best_state)
-print('# SA best fitness: ', sa_best_fitness)
+sa_time = time.perf_counter() - start_time
+print("SA best fitness: {0:.0f} in {1:.4f} seconds and {2} iterations".format(sa_best_fitness, sa_time, len(sa_curve)))
+# print('SA best state:\n', sa_best_state)
 
 # GA
-ga_pop_size = 200
+ga_pop_size = 1000
 ga_mutation_prob = 0.1
+ga_max_attempts = 30
+start_time = time.perf_counter()
 ga_best_state, ga_best_fitness, ga_curve = mlrose.genetic_alg(problem,
                                                               pop_size=ga_pop_size,
                                                               mutation_prob=ga_mutation_prob,
-                                                              max_attempts=max_attempts,
+                                                              max_attempts=ga_max_attempts,
                                                               max_iters=max_iters,
                                                               curve=True,
                                                               random_state=seed)
-#print('# GA best state:\n', ga_best_state)
-print('# GA best fitness: ', ga_best_fitness)
+ga_time = time.perf_counter() - start_time
+print("GA fitness {0:.0f} in {1:.4f} seconds and {2} iterations".format(ga_best_fitness, ga_time, len(ga_curve)))
+# print('GA best state:\n', ga_best_state)
 
 # MIMIC
-mimic_pop_size = 200
-mimic_keep_pct = 0.2
+mimic_pop_size = 1000
+mimic_keep_pct = 0.4
+mimic_max_attempts = 40
+start_time = time.perf_counter()
 mimic_best_state, mimic_best_fitness, mimic_curve = mlrose.mimic(problem,
                                                                  pop_size=mimic_pop_size,
                                                                  keep_pct=mimic_keep_pct,
-                                                                 max_attempts=max_attempts,
+                                                                 max_attempts=mimic_max_attempts,
                                                                  max_iters=max_iters,
                                                                  curve=True,
                                                                  random_state=seed)
-#print('# MIMC best state:\n', mimic_best_state)
-print('# MIMIC best fitness: ', mimic_best_fitness)
+mimic_time = time.perf_counter() - start_time
+print("MIMIC fitness {0:.0f} in {1:.4f} seconds and {2} iterations".format(mimic_best_fitness, mimic_time, len(mimic_curve)))
+# print('MIMIC best state:\n', mimic_best_state)
