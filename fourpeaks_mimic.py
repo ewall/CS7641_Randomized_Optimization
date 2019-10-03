fb7fffce7286c5e7a3a01464332e597a3a6f2408
Eric W. Wallace <e@ewall.org>
2019-10-02 21:59:03 -0400

    added fourpeaks_mimic runner

---
+++ fourpeaks_mimic.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python -W ignore::DeprecationWarning
+
+import mlrose
+import numpy as np
+
+OUTPUT_DIRECTORY = 'experiments'
+SEED = 1
+
+# Four Peaks problem -- with length 40, there are two local peaks of 40, and two optima of 75
+fitness = mlrose.FourPeaks(t_pct=0.1)
+problem = mlrose.DiscreteOpt(length=40, fitness_fn=fitness, maximize=True, max_val=2)
+
+# GA runner
+experiment_name = 'FourPeaks_MIMIC'
+mimic = mlrose.runners.MIMICRunner(problem=problem,
+              experiment_name=experiment_name,
+              output_directory=OUTPUT_DIRECTORY,
+              seed=SEED,
+              iteration_list=10 * np.arange(5) + 10,
+              max_attempts=500,
+              population_sizes=[250, 500, 750, 1000],
+              keep_percent_list=[0.25, 0.5, 0.75])
+df_run_stats, df_run_curves =  mimic.run()
