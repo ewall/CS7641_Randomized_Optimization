f3ad34cd99039a3518bd11a971c520ba0b561c36
Eric W. Wallace <e@ewall.org>
2019-10-13 12:06:42 -0400

    added brute force for knapsack problem

---
+++ brute_knapsack.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# Project 2: Randomized Optimization -- GT CS7641 Machine Learning, Fall 2019
+# Eric W. Wallace, ewallace8-at-gatech-dot-edu, GTID 903105196
+
+import mlrose
+import numpy as np
+import time
+from itertools import product
+
+SEED = 1
+
+# 0-1 Knapsack Problem: can only choose each item once, trying to maximize value before filling up the sack
+weights = [10, 5, 2, 8, 15, 11, 4, 7, 1, 20]
+values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+max_weight_pct = 0.6
+fitness = mlrose.Knapsack(weights, values, max_weight_pct)
+perfect_score = 45
+
+start_time = time.perf_counter()
+for s in product([0,1], repeat=10):
+	f = fitness.evaluate(np.array(s))
+	print(f, s)
+	if f == perfect_score:
+		break
+run_time = time.perf_counter() - start_time
+
+print("\n Solved in", run_time, "seconds.")
